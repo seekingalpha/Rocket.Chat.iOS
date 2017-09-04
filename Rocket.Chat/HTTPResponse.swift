ad1b3c996a4ed8676d1c49c02e852e47a01ab691
Alexander Bugara <alexander@seekingalpha.com>
2017-09-04 03:24:08 +0300

    fix cherry-pick added HTTPResponse and HTTPComplition in SocketManager

---
+++ Rocket.Chat/HTTPResponse.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPResponse.swift
+//  Rocket.Chat
+//
+//  Created by Alexander Bugara on 8/23/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import Foundation
+
+public struct HTTPResponse {
+    var isError = false
+}
