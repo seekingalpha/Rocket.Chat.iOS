54edb602452eaf9e9dfcb269a79f9bae440968ed
Alexander Bugara <alexander@seekingalpha.com>
2017-12-05 14:02:26 +0200

    removed unnecessary Placeholder.m

--- Placeholder.m
+++
@@ -1,9 +0,0 @@
-//
-//  Placeholder.m
-//  Rocket.Chat
-//
-//  Created by Alexander Bugara on 12/5/17.
-//  Copyright © 2017 Rocket.Chat. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
